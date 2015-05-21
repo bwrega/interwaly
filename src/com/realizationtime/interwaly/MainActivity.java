2b1d260004d4a0c4a49a69bafc2e174216af7065
Bartlomiej Wrega <bwrega@gmail.com>
2015-05-21 17:18:23 +0200

    coś się klika

---
+++ src/com/realizationtime/interwaly/MainActivity.java
@@ -0,0 +1,26 @@
+package com.realizationtime.interwaly;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    int licznik = 0;
+    public void onAddBiegaj(View view) {
+        LinearLayout podInterwaly = (LinearLayout) findViewById(R.id.podInterwaly);
+        Button addedButton = new Button(getApplicationContext());
+        licznik++;
+        addedButton.setText("Dodany + "+licznik);
+        podInterwaly.addView(addedButton);
+    }
+}
